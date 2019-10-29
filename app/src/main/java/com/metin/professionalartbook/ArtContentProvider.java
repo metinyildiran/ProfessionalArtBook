6609e731bb50a00ec97e51f883624a88554daa05
Metin Yıldıran <metin.1464@gmail.com>
2019-10-29 16:03:23 +0300

    Initial commit

---
+++ app/src/main/java/com/metin/professionalartbook/ArtContentProvider.java
@@ -0,0 +1,78 @@
+package com.metin.professionalartbook;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ArtContentProvider extends ContentProvider {
+
+    private SQLiteDatabase sqLiteDatabase;
+    static final String DATABASE_NAME = "Arts";
+    static final String ARTS_TABLE_NAME = "arts";
+    static final int DATABASE_VERSION = 1;
+    static final String CREATE_DATABASE_TABLE = "CREATE TABLE " + ARTS_TABLE_NAME + "(name TEXT NOT NULL, " + "image BLOB NOT NULL);";
+
+    private static class DatabaseHelper extends SQLiteOpenHelper{
+
+        public DatabaseHelper(@Nullable Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_DATABASE_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + ARTS_TABLE_NAME);
+            onCreate(db);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+
+        Context context = getContext();
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+
+        sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        return (sqLiteDatabase != null);
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
